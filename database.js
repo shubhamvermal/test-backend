5fb10f221c734a8b8d569927b6c7cd3b6564c7fc
mr-verma1024 <shubhamverma1024@gmail.com>
2020-04-19 14:05:48 +0530

    fix  db

--- database.js
+++ database.js
@@ -4,9 +4,12 @@ const mongoose = require('mongoose');
 const assert = require('assert');
 
 // const db_url = process.env.DB_URL; ////127.0.0.1:27017/mongo_test 
-const db_url = "mongodb:mongodb+srv://shubham:$@Mlr201999@shubham-test-trzmg.mongodb.net/test?retryWrites=true&w=majority"
+const db_url = "mongodb+srv://shubham:$@Mlr201999@shubham-test-trzmg.mongodb.net/test?retryWrites=true&w=majority"
 
 // CONNECTION CODE
+
+// const connectdb = async () => {
+
 mongoose.connect(
     db_url,
     {
@@ -16,6 +19,10 @@ mongoose.connect(
         useFindAndModify: true
     },
     function (err, link) {
+        if (err) {
+
+            console.log('err#####', err.message)
+        }
         //check errF
         assert.equal(err, null, 'DB CONNECTION FAILED');
 
@@ -25,3 +32,4 @@ mongoose.connect(
 
     }
 )
+// }
